568f8d19e044590a561c97eb2a9f79f5328e54ec
root <root@localhost.localdomain>
2021-09-30 16:25:12 +0300

    fix grep in 6.sh

--- lab1/6.sh
+++ lab1/6.sh
@@ -3,6 +3,6 @@
 file="/var/log/anaconda/X.log"
 warning_mark="(WW)"
 info_mark="(II)"
-grep -E $warning_mark $file | sed  "s/$warning_mark/Warning:/" > full.log
-grep -E $info_mark $file | sed "s/$info_mark/Information:/" >> full.log
+grep $warning_mark $file | sed  "s/$warning_mark/Warning:/" > full.log
+grep $info_mark $file | sed "s/$info_mark/Information:/" >> full.log
 cat full.log
