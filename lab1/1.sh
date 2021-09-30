c69dde2bf2561ca39c62b2586c414b9e12aa5938
Egor Erov <erove@inbox.ru>
2021-09-30 19:41:48 +0300

    fix bigint

--- lab1/1.sh
+++ lab1/1.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+#max=$1
+#if  [[ "$max" -lt "$2" ]]; then max=$2; fi
+#if  [[ "$max" -lt "$3" ]]; then max=$3; fi
+#echo $max
+
 max=$1
-if  [[ "$max" -lt "$2" ]]; then max=$2; fi
-if  [[ "$max" -lt "$3" ]]; then max=$3; fi
+if [[ ${#max} -lt ${#2} || ( ${#max} -eq ${#2} && $max < $2 ) ]]; then max=$2; fi
+if [[ ${#max} -lt ${#3} || ( ${#max} -eq ${#3} && $max < $3 ) ]]; then max=$3; fi
 echo $max
