07fb82110de31f01fc7ca5374690e093a3f54424
Egor Erov <erove@inbox.ru>
2021-10-06 21:54:20 +0300

    rewrite 1.sh using bc

--- lab1/1.sh
+++ lab1/1.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+#max=$1
+#if  [[ "$max" -lt "$2" ]]; then max=$2; fi
+#if  [[ "$max" -lt "$3" ]]; then max=$3; fi
+#echo $max
+
 max=$1
-if  [[ "$max" -lt "$2" ]]; then max=$2; fi
-if  [[ "$max" -lt "$3" ]]; then max=$3; fi
+if [[ $(echo $max\<$2 | bc) -eq 1 ]]; then max=$2; fi
+if [[ $(echo $max\<$3 | bc) -eq 1 ]]; then max=$3; fi
 echo $max
