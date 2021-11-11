c548dd32a515c1f97c013c86aa15285ce27ac607
Egor Erov <erove@inbox.ru>
2021-11-11 14:34:00 +0300

    save kill cmd

--- lab3/2.sh
+++ lab3/2.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
 echo "./1.sh &" | at now +1 minute
-#./1.sh
+
+(tail -f ./test/report) |
+while true; do
+	read line;
+	echo $line
+done &
+
+kill -9 $(ps -e -o pid,ppid,command | awk -v this=$$ '$2 == this && $3 == "tail" {print $1}')
