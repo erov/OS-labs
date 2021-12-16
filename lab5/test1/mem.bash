0b858d5953412227f64ff52c57384d42d705895f
Egor Erov <erove@inbox.ru>
2021-12-16 03:22:06 +0300

    upl mem.bash

--- lab5/test1/mem.bash
+++ lab5/test1/mem.bash
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+array=()
+step=0
+> report.log
+
+while [ true ]
+do
+	array+=(1 2 3 4 5 6 7 8 9 10)
+	(( step++ ))
+	if ! (( step % 100000 ))
+	then
+		echo "Size: ${#array[@]}" >> report.log
+	fi
+	#break
+done
+
+echo "${#array[@]}"
